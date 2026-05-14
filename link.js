47eecc5e454ecb2e2860b5bc8b7a0a1e01e6eb6d
kaahRomao <kaueromao1989@gamil.com>
2026-05-13 22:12:33 -0300

    adição de novas pages

--- link.js
+++ link.js
@@ -1,11 +1,36 @@
-document.querySelectorAll(".button").forEach((button) => {
-  button.addEventListener("click", () => {
-    alert("Saiba mais");
-  });
-});
+const elemento = {
+  email: document.querySelector("#email"),
+  senha: document.querySelector("#senha"),
+  form: document.querySelector("form"),
+  button: document.querySelector("button"),
+};
 
-document.querySelectorAll(".botao").forEach((button) => {
-  button.addEventListener("click", () => {
-    alert("Mensagem enviada com sucesso!s");
-  });
-});
+const dadosDigitados = {
+  email: " ",
+  senha: " ",
+};
+
+elemento.form.addEventListener(
+  "submit",
+  (event) => {
+    event.preventDefault;
+    getDadosDigitados(elemento.email.value, elemento.senha.value);
+  },
+
+  function getDadosDigitados(email, senha) {
+    dadosDigitados.email = email;
+    dadosDigitados.senha = senha;
+  },
+);
+
+// document.querySelectorAll(".button").forEach((button) => {
+//   button.addEventListener("click", () => {
+//     alert("Saiba mais");
+//   });
+// });
+
+// document.querySelectorAll(".botao").forEach((button) => {
+//   button.addEventListener("click", () => {
+//     alert("Mensagem enviada com sucesso!s");
+//   });
+// });
